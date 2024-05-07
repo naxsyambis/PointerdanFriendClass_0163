eb42e8810607964d2234d000e5839981bc153776
naxsyambis <naxsyambis@gmail.com>
2024-05-07 12:12:26 +0100

    membuat class siswa dan menambahkan class orang. class bersifat privat dengan variabel nama, dan bersifar public dengan variabel void setNama dana friend class

--- Pointerclassw/Pointerclassw.cpp
+++ Pointerclassw/Pointerclassw.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class siswa;
+class orang
+{
+private:
+    string nama;
 
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
 
 int main()
 {
