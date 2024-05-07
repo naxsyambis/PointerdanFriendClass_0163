3ca81910acbfca687dde11396559186f991bd498
naxsyambis <naxsyambis@gmail.com>
2024-05-07 12:28:33 +0100

    class pelajar int nlai bersifat private dan yang bersifat publik pelajar dan di tambah friend void

--- pointer function dua/pointer function dua.cpp
+++ pointer function dua/pointer function dua.cpp
@@ -8,7 +8,15 @@ public:
     void showNilaiPelajar(pelajar& x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
 
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
 
 int main()
 {
